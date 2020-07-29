8294b80d8f93081566bae3bdb8e28d2005c84eff
meenachinmay <chinmayanand896@gmail.com>
2020-07-29 16:40:28 +0530

    login and signup form refractored

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default function SignUp() {
-    return (
-        <div className="flex-col container mx-auto text-center p-3 m-3">
-            <h3 className="text-gray-600 text-2xl mb-5 uppercase">Sign Up</h3>
-            <form>
-                <div className="mb-3">
-                    <input className="w-1/2 text-center font-semibold rounded px-3 py-4 focus:outline-none border" type="email" placeholder="Enter email"/>
-                </div>
-                <div className="mb-3">
-                    <input className="w-1/2 text-center font-semibold rounded px-3 py-4 focus:outline-none border" type="password" placeholder="Enter Password"/>
-                </div>
-                <div>
-                    <button className="bg-green-300 text-white uppercase hover:bg-green-500 w-1/2 text-center font-semibold rounded px-3 py-4 focus:outline-none" type="password">Sign Up</button>
-                </div>
-            </form>
-        </div>
-    )
+class SignUp extends React.Component {
+    
+    onSubmit = formProps => {
+        console.log(formProps);
+    }
+
+    render(){
+        const { handleSubmit } = this.props;
+
+        return (
+            <div className="flex-col container mx-auto text-center p-3 m-3">
+                <h3 className="text-gray-600 text-2xl mb-5 uppercase">Sign Up</h3>
+                <form onSubmit={handleSubmit(this.onSubmit )}>
+                    <fieldset>
+                        <Field
+                            name="email"
+                            type="text"
+                            component="input"
+                            className="py-2 px-2 mb-3 border focus:outline-none rounded"
+                            placeholder="Email"
+                            autoComplete="none"
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <Field
+                            name="password"
+                            type="password"
+                            component="input"
+                            className="py-2 px-2 border focus:outline-none rounded"
+                            placeholder="Password"
+                            autoComplete="none"
+                        />
+                    </fieldset>
+                    <button className="px-5 py-2 text-white border bg-green-500 hover:bg-green-600 rounded mt-3 focus:outline-none">Sign Up</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default reduxForm({ form: 'signup'})(SignUp);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/index';
 
 import App from './components/App';
 import SignUp from './components/auth/SignUp';
@@ -10,11 +13,13 @@ import Welcome from './components/Welcome';
 import './assets/main.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-    <Route path="/signup" exact component={ SignUp }/>
-    <Route path="/signin" exact component={ SignIn }/>
-    <Route path="/welcome" exact component={ Welcome }/>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App />
+      <Route path="/signup" exact component={ SignUp }/>
+      <Route path="/signin" exact component={ SignIn }/>
+      <Route path="/welcome" exact component={ Welcome }/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
