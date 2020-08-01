f91235a93d94d6d1f67a83d5a609408dd57705b5
meenachinmay <chinmayanand896@gmail.com>
2020-08-01 08:55:58 +0530

    header component code refractor

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,30 @@ import { connect } from 'react-redux';
 
 
 class Header extends React.Component {
+
+    renderLinks = () => {
+        if (this.props.auth.authenticated){
+            return (
+                <div className="flex justify-between items-center">
+                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signout'>Sign Out</Link>
+                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/feature'>Feature</Link>
+                </div>
+            );
+        }else {
+            return (
+                <div className="flex justify-between items-center">
+                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signup'>Sign Up</Link>
+                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signin'>Sign In</Link>
+                </div>
+            );
+        }
+    }
+
     render(){
         return(
-            <div className="container mx-auto text-center">
-                <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/welcome'>Redux auth</Link>
-                
-                { !this.props.auth.authenticated ?  
-                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signup'>Sign Up</Link> : ''
-                }
-                
-                { !this.props.auth.authenticated ? 
-                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signin'>Sign In</Link> : ''
-                }
-
-                { this.props.auth.authenticated ? 
-                    <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/signout'>Sign Out</Link> : '' 
-                }
-                
-                <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/feature'>Feature</Link>
+            <div className="flex justify-between items-center container mx-auto text-center">
+                <Link className="px-3 py-3 rounded hover:bg-gray-200 hover:text-gray-700 text-gray-600" to='/welcome'>Welcome</Link>
+                { this.renderLinks() }    
             </div>
         )
     }
